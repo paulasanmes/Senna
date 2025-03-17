8587fb7560cd7e6a21d853aa58c48ec30a008c42
paulasanmes <paulasm01@gmail.com>
2025-03-17 08:55:28 +0100

    Primer commit

---
+++ src/main/java/com/senna/senna/SennaApplication.java
@@ -0,0 +1,14 @@
+package com.senna.senna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SennaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SennaApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/senna/senna/SennaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senna.senna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SennaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
